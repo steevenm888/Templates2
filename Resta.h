b67c237f6b0a7c298023652f65d2d03b3f504a84
Shakira19 <kscofre@espe.edu.ec>
2018-10-10 11:08:02 -0500

    Funcion resta para trabajo modelo

---
+++ Resta.h
@@ -0,0 +1,14 @@
+template <class T>
+	T Suma<T>::restaNumeros(){
+		return n1-n2;
+	}
+template <class T>
+	T Suma<T>::divisionNumeros(){
+		if (n2==0){
+			return n2/n1;
+		}
+		else
+		{
+			return n1/n2;
+		}	
+	}		
